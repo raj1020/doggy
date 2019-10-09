8b2cfdb307b5dc0259dceb224a71f6ea1991ec63
raj1020 <raj.1020@hotmail.com>
2019-10-09 13:00:58 +1100

    Added 'walk' method in the class to print the number of walks dog had

--- doggy.rb
+++ doggy.rb
@@ -1,17 +1,24 @@
 class Dog
 
     attr_accessor :location
+    @@walk_number = 0
     def initialize(dog_name, dog_age, location)
       @name = dog_name
       @age = dog_age
       @location =location
-      puts "I've been initialized!"
+      
     end
     def speak
       puts "#{@name} says woof! I am #{@age} years old."
+    end
+    def walk
+        @@walk_number +=1
+        return "I have been for #{@@walk_number} walks."
     end
     
   end
   
   doggo = Dog.new('Rover', 2, "Brisbane")
   p doggo.location
+  doggo.walk
+  p doggo.walk
